a93b9c87444cda23807f289d56521f6e219cfc0e
Divya R <kwrdivyaraj100@gmail.com>
2023-11-29 18:10:29 +0530

    Converting to lowercase funtionality

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -7,20 +7,27 @@ function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    function handleLoClick(){
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
     function handleOnChange(event){
         setText(event.target.value)
     }
   return (
     <>
-    <h1>{props.heading}</h1>
+    <div className='container'>
     <div className='mb-3'>
+    <h1>{props.heading}</h1>
         <textarea value={text} onChange={handleOnChange} className='form-control' id='myBox' rows="8"></textarea>
         </div>
         <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className='btn btn-primary' onClick={handleLoClick}>Convert to Lowercase</button>
+        </div>
         <div className='container my-3'>
             <h1>Your Text Summary</h1>
             <p>{text.split(" ").length} words & {text.length} characters</p>
-            <p>{0.7* text.split(" ").length}seconds to read</p>
+            <p>{0.7 * text.split(" ").length}seconds to read</p>
             <h2>Preview</h2>
             <p>{text}</p>
         </div>
