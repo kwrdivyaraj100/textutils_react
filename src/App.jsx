6fd03cc51daf3c9a9405f8937f43dbbea13ef241
Divya R <kwrdivyaraj100@gmail.com>
2023-11-29 17:29:09 +0530

    Adding bootstrap container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
-     {/* <Navbar title = "Text Util" about = "About Text Utilities" /> */}
-    <Navbar/>
+     <Navbar title = "Text Util" about = "About Text Utilities" />/
+    <div className="container">
+      <TextForm/>
+    </div>
     </>
   )
 }
