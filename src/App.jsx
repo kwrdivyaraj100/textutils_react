34c09b33a05bd33ec5fa94203ae1076273c47d8f
Divya R <kwrdivyaraj100@gmail.com>
2023-11-29 18:26:38 +0530

    Adding accordion inside a container

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
-     <Navbar title = "Text Util" about = "About Text Utilities" />/
+     {/* <Navbar title = "Text Util" about = "About Text Utilities" />/
     <div className="container">
-      <TextForm heading="Enter the text to analyze"/>
-    </div>
+      <TextForm heading="Enter the text to analyze"/> */}
+      <About/>
+    {/* </div> */}
     </>
   )
 }
