451d1f092f98f796090fcc8be1e2edfe2ab30e5b
Divya R <kwrdivyaraj100@gmail.com>
2023-11-29 18:01:37 +0530

    more features on text preview

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -17,6 +17,13 @@ function TextForm(props) {
         <textarea value={text} onChange={handleOnChange} className='form-control' id='myBox' rows="8"></textarea>
         </div>
         <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
+        <div className='container my-3'>
+            <h1>Your Text Summary</h1>
+            <p>{text.split(" ").length} words & {text.length} characters</p>
+            <p>{0.7* text.split(" ").length}seconds to read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
         </>
   )
 }
