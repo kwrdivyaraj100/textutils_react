fbda12b7b15cb4262d1e70604d4e79e975deb168
Divya R <kwrdivyaraj100@gmail.com>
2023-11-29 17:50:16 +0530

    Adding Use State

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function TextForm(props) {
+    const [text, setText] = useState('Enter Text Here');
+
+    function handleUpClick(){
+        console.log("On up Click");
+    }
+    function handleOnChange(){
+        console.log("on change");
+    }
   return (
     <>
     <h1>{props.heading}</h1>
     <div className='mb-3'>
-        <textarea className='form-control' id='myBox' rows="8"></textarea>
+        <textarea value={text} onChange={handleOnChange} className='form-control' id='myBox' rows="8"></textarea>
         </div>
         <button className='btn btn-primary'>Convert to Uppercase</button>
         </>
