8250ca95b98f770a30d1eb85de34b206d55c59e0
Divya R <kwrdivyaraj100@gmail.com>
2023-11-30 17:03:58 +0530

    Adding useEffect hook to toggle dark mode in document body

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,20 @@ import Alert from "./components/Alert";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light');
+  const [state, setState] = useState({
+    darkMode: false,
+    alert: null
+  });
 
-  const [alert, setAlert] = useState(null);
+  useEffect(() => {
+    if (state.darkMode) {
+      document.body.style.backgroundColor = '#343a40';
+      document.body.style.color = 'white';
+    } else {
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = '#343a40';
+    }
+  }, [state.darkMode]);
 
   function showAlert(message, type) {
     setAlert({
@@ -31,16 +42,16 @@ function App() {
   }
   return (
     <>
-    
+
       <Navbar title="Text Util" about="About Text Utilities" mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert} />
       <div className="container my-3">
-        </div>
-        <Routes>
-          <Route path='/' element={<TextForm heading="Enter the text to analyze below" showAlert={showAlert} />} />
-          <Route path='/about' element={<About darkMode={state.darkMode} />} />
-        </Routes>
-        
+      </div>
+      <Routes>
+        <Route path='/' element={<TextForm heading="Enter the text to analyze below" showAlert={showAlert} />} />
+        <Route path='/about' element={<About darkMode={state.darkMode} />} />
+      </Routes>
+
     </>
   )
 }
