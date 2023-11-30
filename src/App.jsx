0bbf455c8eaeb6d1ab93babe77c4eabe80d0625f
Divya R <kwrdivyaraj100@gmail.com>
2023-11-30 13:05:23 +0530

    Adding Alert State in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,39 @@ import { useState } from "react";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
 
 function App() {
   const [mode, setMode] = useState('light');
 
-  function toggleMode(){
+  const [alert, setAlert] = useState(null);
+
+  function showAlert(message, type) {
+    setAlert({
+      msg: message,
+      type: type
+    })
+  }
+
+  function toggleMode() {
     if (mode === 'light') {
       setMode('dark');
-      document.body.style.background = '#495057'
-    }else{
+      document.body.style.background = '#042743';
+      showAlert("Dark Mode has been Enabled", "success");
+    } else {
       setMode('light');
-      document.body.style.background = '#ffffff'
-  };
-}
+      document.body.style.background = '#ffffff';
+      showAlert("Dark Mode has been Enabled", "success");
+    };
+  }
   return (
     <>
-     <Navbar title = "Text Util" about = "About Text Utilities" mode={mode} toggleMode={toggleMode} />
-    <div className="container my-3">
-      <TextForm heading="Enter the text to analyze" mode={mode}/>
-      <About/>
-    </div>
+      <Navbar title="Text Util" about="About Text Utilities" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <TextForm heading="Enter the text to analyze" mode={mode} />
+        <About />
+      </div>
     </>
   )
 }
