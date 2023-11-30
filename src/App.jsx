79166df5ae1faa6e46bec48477a19e171dbe37d6
Divya R <kwrdivyaraj100@gmail.com>
2023-11-30 11:53:45 +0530

    Applying Props in Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
 function App() {
+  const [mode, setMode] = useState('light');
   return (
     <>
      <Navbar title = "Text Util" about = "About Text Utilities" mode={mode} />
